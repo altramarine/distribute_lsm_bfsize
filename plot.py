9da9315920b5279a086f1e80ad908c4c48cb8754
littlepig2013 <zhuzichen19960417@gmail.com>
2024-02-08 21:31:41 -0500

    adding fonts

--- plot.py
+++ plot.py
@@ -3,13 +3,16 @@ import os
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+import matplotlib.font_manager as font_manager
 
-# mpl.rcParams['text.usetex'] = True
-plt.rc('font',family='Linux Libertine Mono', size=15)
 
-root_dir = "./varyN_bpk6_E128_ED_Q4M"
+prop = font_manager.FontProperties(fname="./fonts/LinLibertine_Mah.ttf")
+mpl.rcParams['font.family'] = prop.get_name()
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams.update({'font.size': 23})
+#plt.rc('font',family='Linux Libertine Mono', size=15)
+
 BPK = [1, 2, 3, 4, 5, 6, 7, 8]
-dir_lists_1 = os.listdir(root_dir)
 # Zlist = ["Z0.0_ZD0_query_stats.txt", "Z0.25_ZD0_query_stats.txt", "Z0.5_ZD0_query_stats.txt", "Z0.75_ZD0_query_stats.txt", "Z1.0_ZD0_query_stats.txt"]
 Ns = [1, 2, 4, 8, 16, 32]
 
@@ -19,6 +22,8 @@ print(df)
 algos = ["monkey", "binarysearch", "scan"]
 algoname = ["gradiant descent", "binary search", "scan"]
 lines = ["--", "-.", "-"]
+linecolors = ["black", "blue", "red"]
+linemarkers = ["o", "s", "x"]
 def plot_for_n_as_xaxis_split_bpk():
   plt.figure(f'run_for_n, split_n_bpk',figsize=(12 * 3, 9 * 8))
   for bpk in BPK:
@@ -41,7 +46,7 @@ def plot_for_n_as_xaxis_split_bpk():
               x.append(_x)
               y.append(_y)
           plt.subplot(8, 3, bpk * 3 + i - 2)
-          plt.title(f"bpk={bpk}")
+          #plt.title(f"bpk={bpk}")
           plt.plot(x, y, color = Col(int(z * 10 + 3) / 14), linestyle = line, label = f"z={z}, zd={zd}, {algo}")
           plt.xlabel("n(M)")
           plt.ylabel("time")
@@ -50,7 +55,8 @@ def plot_for_n_as_xaxis_split_bpk():
           plt.subplot(8, 3, bpk * 3 + i - 2).legend()
   # plt.plot()
   # plt.show()
-  plt.savefig("./fig/split_bpk.png")
+  plt.tight_layout()
+  plt.savefig("./fig/split_bpk.pdf", bbox_inches = "tight", dpi=900)
 
 
 
@@ -61,6 +67,7 @@ def plot_for_linear_plot(bpk: int, zd = 0):
   ____zipf ='zipf'
   # plt.figure(f'z = [0.0, 0.5, 1.0], {____uniform if zd==0 else ____zipf}, bpk = {bpk}', figsize=(12 * length, 9 * 1))
   ii = 0
+  entries_per_file = 262144
   for z in allZ:
     plt.figure(f'Z{z:.1f}_{____uniform if zd==0 else ____zipf}_bpk{bpk}', figsize = (8, 6))
     ii += 1
@@ -73,23 +80,26 @@ def plot_for_linear_plot(bpk: int, zd = 0):
       y = []
       for n in Ns:
         if(len(_df[_df["N"] == n]) > 0):
-          _x = n * 1000000;
+          _x = int(round((n * 1000000)/entries_per_file))
           _y = np.average(_df[_df["N"] == n][f't_{algo}'])
           x.append(_x)
           y.append(_y)
       # plt.subplot(1, length, ii)
-      plt.title(f"Z{z:.1f}_{____uniform if zd==0 else ____zipf}_bpk{bpk}")
+      #plt.title(f"Z{z:.1f}_{____uniform if zd==0 else ____zipf}_bpk{bpk}")
       # plt.plot(x, y, linestyle = line, label = f"{name}")
-      plt.plot(x, y, label = f"{name}")
-      plt.ticklabel_format(axis="x", style="sci", scilimits=(6,6))
+      plt.plot(x, y, label = f"{name}", color=linecolors[i], marker=linemarkers[i], markersize=16, fillstyle='none')
+      plt.ticklabel_format(axis="x", style="sci", scilimits=(0,6))
       plt.yscale("log")
-      plt.xlabel("n")
-      plt.ylabel("time")
+      plt.ylim(1e-6, 100)
+      plt.yticks([1e-6,1e-4,0.01,1,100])
+      plt.xlabel("\#SST files ($F$)")
+      plt.ylabel("time (seconds)")
       # plt.ylim(0)
     # for N, ____df in ___df.groupby(___df["N"]):
     plt.legend()
     # plt.subplot(1, length, ii).legend()
-    plt.savefig(f"./fig/split/Z{z:.1f}_{____uniform if zd==0 else ____zipf}_bpk{bpk}.png")
+    plt.tight_layout()
+    plt.savefig(f"./fig/split/Z{z:.1f}_{____uniform if zd==0 else ____zipf}_bpk{bpk}.pdf", bbox_inches = "tight", dpi=900)
   # plt.savefig(f"./fig/{____uniform if zd==0 else ____zipf},bpk={bpk}.png")
   # plt.savefig("./fig/split_n.png")
 
@@ -124,7 +134,8 @@ def plot_for_bpk_as_xaxis_split_n():
           plt.ylabel("time")
           # plt.ylim(0)
           plt.subplot(6, 3, ii * 3 + i - 2).legend()
-  plt.savefig("./fig/split_n.png")
+  plt.tight_layout()
+  plt.savefig("./fig/split_n.pdf", bbox_inches = "tight", dpi=900)
   # plt.show()
 
 
@@ -132,8 +143,9 @@ def plot_for_bpk_as_xaxis_split_n():
 def main():
   for bpk in range(1, 8 + 1):
     plot_for_linear_plot(bpk = bpk, zd = 0)
-  plot_for_n_as_xaxis_split_bpk()
-  plot_for_bpk_as_xaxis_split_n()
+    plot_for_linear_plot(bpk = bpk, zd = 3)
+  #plot_for_n_as_xaxis_split_bpk()
+  #plot_for_bpk_as_xaxis_split_n()
 
 if __name__ == "__main__":
   main()

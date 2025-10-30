b6bb112dc72fc41d4c8e1f99068919943ce5557d
littlepig2013 <zhuzichen19960417@gmail.com>
2025-10-29 21:00:29 -0400

    fix python version

--- run_scripts.sh
+++ run_scripts.sh
@@ -13,8 +13,8 @@ mkdir -p exp-figures
 REPEAT_NUM=3                # repeated times for each dataset
 TEST_BPK=(2 4 8)            # list of bits-per-key to be tested
 
-python ./run_experiments.py -d $TEST_DIR -r $REPEAT_NUM --bpk ${TEST_BPK[@]} 2> exp-log.txt
-python ./plot_N-time_foreach_bpk.py --bpk ${TEST_BPK[@]} -d ${OUTPUT_DIR} 2> plt-log.txt
+python3 ./run_experiments.py -d $TEST_DIR -r $REPEAT_NUM --bpk ${TEST_BPK[@]} 2> exp-log.txt
+python3 ./plot_N-time_foreach_bpk.py --bpk ${TEST_BPK[@]} -d ${OUTPUT_DIR} 2> plt-log.txt
 
 mkdir -p ../exp-figures
 cp ${OUTPUT_DIR}/Z0.0_uniform_bpk2.pdf ../exp-figures/Fig5-a.pdf
